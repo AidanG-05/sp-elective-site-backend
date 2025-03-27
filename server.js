c1b35b43207cfe6bfe6fb3e828fccf012ac6eb2a
Theyan <rstheyanashvaran17.22@ichat.sp.edu.sg>
2025-03-27 13:11:37 +0800

    Was able to establish connection with the AWS server

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get('/modules', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3306;
 console.log("Server script started!"); // Add this at the top
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
