a92c9f10d7cacf5ab12bc86cd27c2bac63afb897
Theyan <rstheyanashvaran17.22@ichat.sp.edu.sg>
2025-04-05 00:31:14 +0800

    Backend done for Module info and reviews

--- server.js
+++ server.js
@@ -58,6 +58,37 @@ app.get('/modules/all', (req, res) => {
 
 })
 
+//fetch module by module code
+app.get('/modules/:module_code', (req, res) => {
+  const moduleCode = req.params.module_code;
+  const sql = 'SELECT * FROM module WHERE module_code = ?';
+
+  db.query(sql, [moduleCode], (err, results) => {
+      if (err) {
+          return res.status(500).json({ error: err.message });
+      }
+      if (results.length === 0) {
+          return res.status(404).json({ error: "Module not found" });
+      }
+      res.json(results[0]);
+  });
+});
+
+//fetch reviews by module code
+app.get('/modules/:module_code/reviews', (req, res) => {
+  const moduleCode = req.params.module_code;
+
+  const sql = `SELECT * FROM user_reviews WHERE Elective_Code = ?`;
+
+  db.query(sql, [moduleCode], (err, results) => {
+      if (err) {
+          console.error("Error fetching reviews:", err);
+          return res.status(500).json({ error: "Failed to fetch reviews" });
+      }
+      res.json(results);
+  });
+});
+
 // Start the server
 const PORT = process.env.PORT || 5001;
 console.log("Server script started!");
